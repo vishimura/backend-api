fbc8d299fe9cdefb2e9c64f78f85a01a684ab1e8
vishimura <vitor.shimura@hotmail.com>
2019-04-23 19:58:52 -0300

    Configurações do servidor e rotas

---
+++ users/users.model.ts
@@ -0,0 +1,23 @@
+
+
+const users = [
+    {id: '1', name: 'Peter Parker', email: 'peter@marvel.com'},
+    {id: '2', name: 'Bruce Wayne', email: 'batman@dc.com'}
+
+]
+
+export class User {
+    static findAll(): Promise<any[]>{
+        return Promise.resolve(users)
+    }
+    static findById(id: string): Promise<any>{
+        return new Promise(resolve => {
+            const filtered = users.filter(users => users.id === id)
+            let user = undefined
+            if(filtered.length > 0){
+                user = filtered[0]
+            }
+            resolve(user)
+        })
+    }
+}
